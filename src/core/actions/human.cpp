6834a1335855d301af25278ac73ad99fd80c1802
imaroger <imaroger@laas.fr>
2020-01-23 11:32:18 +0100

    working and cleaner version of human model

--- src/core/actions/human.cpp
+++ src/core/actions/human.cpp
@@ -5,115 +5,6 @@
 
 namespace crocoddyl {
 
-
-// ActionModelHuman::ActionModelHuman() : ActionModelAbstract(boost::make_shared<StateVector>(3), 2, 5), dt_(0.1) {
-//   cost_weights_ << 10., 1.;
-// }
-
-// ActionModelHuman::~ActionModelHuman() {}
-
-// void ActionModelHuman::calc(const boost::shared_ptr<ActionDataAbstract>& data,
-//                               const Eigen::Ref<const Eigen::VectorXd>& x,
-//                               const Eigen::Ref<const Eigen::VectorXd>& u){
-//   if (static_cast<std::size_t>(x.size()) != state_->get_nx()) {
-//     throw_pretty("Invalid argument: "
-//                  << "x has wrong dimension (it should be " + std::to_string(state_->get_nx()) + ")");
-//   }
-//   if (static_cast<std::size_t>(u.size()) != nu_) {
-//     throw_pretty("Invalid argument: "
-//                  << "u has wrong dimension (it should be " + std::to_string(nu_) + ")");
-//   }
-
-//   ActionDataHuman* d = static_cast<ActionDataHuman*>(data.get());
-//   const double& c = std::cos(x[2]);
-//   const double& s = std::sin(x[2]);
-//   d->xnext << x[0] + c * u[0] * dt_, x[1] + s * u[0] * dt_, x[2] + u[1] * dt_;
-//   // d->r.head<3>() = cost_weights_[0] * x;
-//   // d->r.tail<2>() = cost_weights_[1] * u;
-//   //d->cost = 0.5 * d->r.transpose() * d->r;
-//   d->cost = costFunction(x[0],x[1],x[2],u[0],u[1]);
-// }
-
-// double ActionModelHuman::costFunction(double x, double y, double theta, 
-//                                         double u1, double u2){
-//   double cost;
-//   cost = (pow(cost_weights_[0]*x,2) + pow(cost_weights_[0]*y,2) + 
-//     pow(cost_weights_[0]*theta,2) + pow(cost_weights_[1]*u1,2) + 
-//     pow(cost_weights_[1]*u2,2))/2;
-//   return cost;
-// }
-
-// void ActionModelHuman::calcDiff(const boost::shared_ptr<ActionDataAbstract>& data,
-//                                    const Eigen::Ref<const Eigen::VectorXd>& x,
-//                                    const Eigen::Ref<const Eigen::VectorXd>& u, const bool& recalc) {
-//   if (static_cast<std::size_t>(x.size()) != state_->get_nx()) {
-//     throw_pretty("Invalid argument: "
-//                  << "x has wrong dimension (it should be " + std::to_string(state_->get_nx()) + ")");
-//   }
-//   if (static_cast<std::size_t>(u.size()) != nu_) {
-//     throw_pretty("Invalid argument: "
-//                  << "u has wrong dimension (it should be " + std::to_string(nu_) + ")");
-//   }
-
-//   if (recalc) {
-//     calc(data, x, u);
-//   }
-//   ActionDataHuman* d = static_cast<ActionDataHuman*>(data.get());
-
-//   // Cost derivatives
-//   d->Lx << (costFunction(x[0]+1e-6,x[1],x[2],u[0],u[1]) - costFunction(x[0],x[1],x[2],u[0],u[1]))/1e-6,
-//     (costFunction(x[0],x[1]+1e-6,x[2],u[0],u[1]) - costFunction(x[0],x[1],x[2],u[0],u[1]))/1e-6,
-//     (costFunction(x[0],x[1],x[2]+1e-6,u[0],u[1]) - costFunction(x[0],x[1],x[2],u[0],u[1]))/1e-6;
-
-//   std::cout << d->Lx << "| \n" << x.cwiseProduct(Eigen::VectorXd::Constant(state_->get_nx(), cost_weights_[0]*cost_weights_[0])) << std::endl;  
-//   std::cout << "------" << std::endl;
-
-//   d->Lu << (costFunction(x[0],x[1],x[2],u[0]+1e-6,u[1]) - costFunction(x[0],x[1],x[2],u[0],u[1]))/1e-6,
-//     (costFunction(x[0],x[1],x[2],u[0],u[1]+1e-6) - costFunction(x[0],x[1],x[2],u[0],u[1]))/1e-6;
-  
-//   std::cout << d->Lu << "| \n" << u.cwiseProduct(Eigen::VectorXd::Constant(nu_, cost_weights_[1]*cost_weights_[1])) << std::endl;  
-//   std::cout << "------" << std::endl;
-
-//   d->Lxx << (costFunction(x[0]+1e-6,x[1],x[2],u[0],u[1]) - 2*costFunction(x[0],x[1],x[2],u[0],u[1]) 
-//     + costFunction(x[0]-1e-6,x[1],x[2],u[0],u[1]))/pow(1e-6,2), // dl/dxdx
-//     (costFunction(x[0]+1e-6,x[1]+1e-6,x[2],u[0],u[1]) - costFunction(x[0]+1e-6,x[1],x[2],u[0],u[1]) 
-//     - costFunction(x[0],x[1]+1e-6,x[2],u[0],u[1]) + costFunction(x[0],x[1],x[2],u[0],u[1]))/pow(1e-6,2), // dl/dxdy
-//     (costFunction(x[0]+1e-6,x[1],x[2]+1e-6,u[0],u[1]) - costFunction(x[0]+1e-6,x[1],x[2],u[0],u[1]) 
-//     - costFunction(x[0],x[1],x[2]+1e-6,u[0],u[1]) + costFunction(x[0],x[1],x[2],u[0],u[1]))/pow(1e-6,2), // dl/dxdz
-//     (costFunction(x[0]+1e-6,x[1]+1e-6,x[2],u[0],u[1]) - costFunction(x[0]+1e-6,x[1],x[2],u[0],u[1]) 
-//     - costFunction(x[0],x[1]+1e-6,x[2],u[0],u[1]) + costFunction(x[0],x[1],x[2],u[0],u[1]))/pow(1e-6,2), // dl/dydx 
-//     (costFunction(x[0],x[1]+1e-6,x[2],u[0],u[1]) - 2*costFunction(x[0],x[1],x[2],u[0],u[1]) 
-//     + costFunction(x[0],x[1]-1e-6,x[2],u[0],u[1]))/pow(1e-6,2), // dl/dydy 
-//     (costFunction(x[0],x[1]+1e-6,x[2]+1e-6,u[0],u[1]) - costFunction(x[0],x[1],x[2]+1e-6,u[0],u[1]) 
-//     - costFunction(x[0],x[1]+1e-6,x[2],u[0],u[1]) + costFunction(x[0],x[1],x[2],u[0],u[1]))/pow(1e-6,2), // dl/dydz 
-//     (costFunction(x[0]+1e-6,x[1],x[2]+1e-6,u[0],u[1]) - costFunction(x[0]+1e-6,x[1],x[2],u[0],u[1]) 
-//     - costFunction(x[0],x[1],x[2]+1e-6,u[0],u[1]) + costFunction(x[0],x[1],x[2],u[0],u[1]))/pow(1e-6,2), // dl/dzdx
-//     (costFunction(x[0],x[1]+1e-6,x[2]+1e-6,u[0],u[1]) - costFunction(x[0],x[1],x[2]+1e-6,u[0],u[1]) 
-//     - costFunction(x[0],x[1]+1e-6,x[2],u[0],u[1]) + costFunction(x[0],x[1],x[2],u[0],u[1]))/pow(1e-6,2), // dl/dzdy
-//     (costFunction(x[0],x[1],x[2]+1e-6,u[0],u[1]) - 2*costFunction(x[0],x[1],x[2],u[0],u[1]) 
-//     + costFunction(x[0],x[1],x[2]-1e-6,u[0],u[1]))/pow(1e-6,2); // dl/dzdz
-
-//   std::cout << d->Lxx << std::endl;
-//   std::cout << "------" << std::endl;    
-
-//   d->Luu << (costFunction(x[0],x[1],x[2],u[0]+1e-6,u[1]) - 2*costFunction(x[0],x[1],x[2],u[0],u[1]) 
-//     + costFunction(x[0],x[1],x[2],u[0]-1e-6,u[1]))/pow(1e-6,2), // dl/du1du1
-//     (costFunction(x[0],x[1],x[2],u[0]+1e-6,u[1]+1e-6) - costFunction(x[0],x[1],x[2],u[0]+1e-6,u[1]) 
-//     - costFunction(x[0],x[1],x[2],u[0],u[1]+1e-6) + costFunction(x[0],x[1],x[2],u[0],u[1]))/pow(1e-6,2), // dl/du1du2
-//     (costFunction(x[0],x[1],x[2],u[0]+1e-6,u[1]+1e-6) - costFunction(x[0],x[1],x[2],u[0]+1e-6,u[1]) 
-//     - costFunction(x[0],x[1],x[2],u[0],u[1]+1e-6) + costFunction(x[0],x[1],x[2],u[0],u[1]))/pow(1e-6,2), // dl/du2du1 
-//     (costFunction(x[0],x[1],x[2],u[0],u[1]+1e-6) - 2*costFunction(x[0],x[1],x[2],u[0],u[1]) 
-//     + costFunction(x[0],x[1],x[2],u[0],u[1]-1e-6))/pow(1e-6,2); // dl/du2du2 
-//   std::cout << d->Luu << std::endl;
-//   std::cout << "------" << std::endl;  
-
-//   // Dynamic derivatives
-//   const double& c = std::cos(x[2]);
-//   const double& s = std::sin(x[2]);
-//   d->Fx << 1., 0., -s * u[0] * dt_, 0., 1., c * u[0] * dt_, 0., 0., 1.;
-//   d->Fu << c * dt_, 0., s * dt_, 0., 0., dt_;
-// }
-
 ActionModelHuman::ActionModelHuman() : ActionModelAbstract(boost::make_shared<StateVector>(6), 3, 5), dt_(0.01) {
   cost_weights_ << 1., 1.2, 1.7, 0.7, 5.2, 1. , 1.;
   final_state_ << 0., 0., 0.;
@@ -142,11 +33,6 @@ void ActionModelHuman::calc(const boost::shared_ptr<ActionDataAbstract>& data,
     x[3] + u[0]*dt_,
     x[4] + u[1]*dt_,
     x[5] + u[2]*dt_;
-  // d->r << sqrt(cost_weights_[0]),
-  //   sqrt(cost_weights_[1])*u[0],
-  //   sqrt(cost_weights_[2])*u[1],
-  //   sqrt(cost_weights_[3])*u[2],
-  //   sqrt(cost_weights_[4])*atan((final_state_[1]-x[1])/(final_state_[0]-x[0]))-x[2];
   d->cost = costFunction(x[0],x[1],x[2],u[0],u[1],u[2]); 
 
   std::cout << "cost : " << d->cost << std::endl;
@@ -156,25 +42,22 @@ double ActionModelHuman::costFunction(double x, double y, double theta,
                                         double u1, double u2, double u3){
   double cost;
   double diff_theta = fmod(final_state_[2],M_PI) - fmod(theta,M_PI);
-  double diff_theta_final = diff_theta + (( diff_theta > M_PI) ? - 2*M_PI : ((diff_theta< -M_PI) ? 2*M_PI: 0));
-  // std::cout << "diff_theta_final " << diff_theta_final 
-  //           << " " << diff_theta << " "
-  //           << final_state_[2] << " " << theta << std::endl;
+  double diff_theta_final = diff_theta + 
+    (( diff_theta > M_PI) ? - 2*M_PI : ((diff_theta< -M_PI) ? 2*M_PI: 0));
 
   cost = 
-    cost_weights_[0] + cost_weights_[1]*pow(u1,2) + cost_weights_[2]*pow(u2,2) +
-    cost_weights_[3]*pow(u3,2) + cost_weights_[4]*
-    pow(atan2(final_state_[1]-y,final_state_[0]-x)-theta,2) + 
+    cost_weights_[0] + cost_weights_[1]*pow(u1,2) + 
+    cost_weights_[2]*pow(u2,2) + cost_weights_[3]*pow(u3,2) + 
+    cost_weights_[4]*pow(atan2(final_state_[1]-y,final_state_[0]-x)-theta,2) + 
     cost_weights_[5]*(pow(final_state_[0]-x,2) + pow(final_state_[1]-y,2)) + 
     cost_weights_[6]*pow(diff_theta_final,2);
-    // (pow(final_state_[0] + cos(final_state_[2]) - x - cos(theta),2) +
-    // pow(final_state_[1] + sin(final_state_[2]) - y + sin(theta),2))); //+ pow(diff_theta_final,2));
   return cost;
 }
 
 void ActionModelHuman::calcDiff(const boost::shared_ptr<ActionDataAbstract>& data,
                                    const Eigen::Ref<const Eigen::VectorXd>& x,
-                                   const Eigen::Ref<const Eigen::VectorXd>& u, const bool& recalc) {
+                                   const Eigen::Ref<const Eigen::VectorXd>& u, 
+                                   const bool& recalc) {
   if (static_cast<std::size_t>(x.size()) != state_->get_nx()) {
     throw_pretty("Invalid argument: "
                  << "x has wrong dimension (it should be " + std::to_string(state_->get_nx()) + ")");
@@ -190,17 +73,6 @@ void ActionModelHuman::calcDiff(const boost::shared_ptr<ActionDataAbstract>& dat
   ActionDataHuman* d = static_cast<ActionDataHuman*>(data.get());
 
   // Cost derivatives
-
-  //const double& psi = atan((final_state_[1]-x[1])/(final_state_[0]-x[0]))-x[2];
-  // Eigen::MatrixXd Lx2;
-  // Eigen::MatrixXd Lu2;  
-  // Lx2 << (final_state_[1]-x[1])*cost_weights_[4]/(pow(final_state_[1]-x[1],2)+
-  //   (pow(final_state_[0]-x[0],2)))*2*psi,
-  //   (final_state_[0]-x[0])*cost_weights_[4]/(pow(final_state_[1]-x[1],2)+
-  //   (pow(final_state_[0]-x[0],2)))*2*psi,
-  //   -2*cost_weights_[4]*psi, 0, 0, 0;
-  //Lu2 << 2*cost_weights_[1]*u[1],2*cost_weights_[2]*u[2],2*cost_weights_[3]*u[3];
-  
   double h = 1e-5;
 
   std::cout << "--- state ---" << std::endl; 
@@ -218,10 +90,6 @@ void ActionModelHuman::calcDiff(const boost::shared_ptr<ActionDataAbstract>& dat
   std::cout << "--- Lx---" << std::endl; 
   std::cout << d->Lx << std::endl;
 
-  // std::cout << "--- Lx derivate ---" << std::endl; 
-  // std::cout << (final_state_[1]-x[1])*cost_weights_[4]/(pow(final_state_[1]-x[1],2)+
-  //   (pow(final_state_[0]-x[0],2)))*2*psi << std::endl; 
-
   // d->Lu << (costFunction(x[0],x[1],x[2],u[0]+h,u[1],u[2]) - costFunction(x[0],x[1],x[2],u[0],u[1],u[2]))/h,
   //   (costFunction(x[0],x[1],x[2],u[0],u[1]+h,u[2]) - costFunction(x[0],x[1],x[2],u[0],u[1],u[2]))/h,
   //   (costFunction(x[0],x[1],x[2],u[0],u[1],u[2]+h) - costFunction(x[0],x[1],x[2],u[0],u[1],u[2]))/h;
@@ -230,9 +98,6 @@ void ActionModelHuman::calcDiff(const boost::shared_ptr<ActionDataAbstract>& dat
 
   std::cout << "--- Lu---" << std::endl; 
   std::cout << d->Lu << std::endl;   
-
-  // std::cout << "--- Lu derivate ---" << std::endl; 
-  // std::cout << Lu2 << std::endl; 
 
   d->Lxx.block(0,0,3,3) << (costFunction(x[0]+h,x[1],x[2],u[0],u[1],u[2]) - 2*costFunction(x[0],x[1],x[2],u[0],u[1],u[2]) 
     + costFunction(x[0]-h,x[1],x[2],u[0],u[1],u[2]))/pow(h,2), // dl/dx²
